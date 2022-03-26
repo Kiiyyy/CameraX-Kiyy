6cd4faa43fa189dee4514acc09ad7e83dbb515ae
Rizki Cahya <rizkiicahyaaa21@gmail.com>
2022-03-26 23:05:16 +0700

    Update MainActivity.kt

--- app/src/main/java/com/rizki/kiyycamerax/MainActivity.kt
+++ app/src/main/java/com/rizki/kiyycamerax/MainActivity.kt
@@ -22,19 +22,14 @@ import java.util.*
 
 typealias LumaListener = (luma: Double) -> Unit
 
-
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
-
     private var imageCapture: ImageCapture? = null
-
     private lateinit var cameraExecutor: ExecutorService
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-
         // Minta kepada pengguna untuk mengizinan Kamera
         if (allPermissionsGranted()) {
             startCamera()
@@ -42,17 +37,13 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
-
         // Mendambahkan Listener untuk mengambil foto
         viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
-
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
-
     private fun takePhoto() {
         // Melakukan Penggambilan Gambar(dapat diulang ulang)
         val imageCapture = imageCapture ?: return
-
         // Buat nama pada media penyimpanan yang digunakan
         val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
             .format(System.currentTimeMillis())
@@ -65,14 +56,12 @@ class MainActivity : AppCompatActivity() {
                 put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/KiyyCameraX-Image")
             }
         }
-
         // Pembuatah Hasil Output dari foto
         val outputOptions = ImageCapture.OutputFileOptions
             .Builder(contentResolver,
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                 contentValues)
             .build()
-
         // Listener disiapkan untuk pengambilan gambar setelah button take foto ditekan
         imageCapture.takePicture(
             outputOptions,
@@ -81,7 +70,6 @@ class MainActivity : AppCompatActivity() {
                 override fun onError(exc: ImageCaptureException) {
                     Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
                 }
-
                 override fun
                         onImageSaved(output: ImageCapture.OutputFileResults){
                     val msg = "Photo capture succeeded: ${output.savedUri}"
@@ -93,33 +81,26 @@ class MainActivity : AppCompatActivity() {
     }
 
      private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
-
         private fun ByteBuffer.toByteArray(): ByteArray {
             rewind()    // Mengulang buffer
             val data = ByteArray(remaining())
             get(data)
             return data
         }
-
         override fun analyze(image: ImageProxy) {
             val rotation = image.imageInfo.rotationDegrees
             val buffer = image.planes[0].buffer
             val data = buffer.toByteArray()
             val pixels = data.map { it.toInt() and 0xFF }
             val luma = pixels.average()
-
             listener(luma)
-
             image.close()
         }
     }
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
         cameraProviderFuture.addListener({
-
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
             // Hasil
             val preview = Preview.Builder()
                 .build()
@@ -127,22 +108,17 @@ class MainActivity : AppCompatActivity() {
                     it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
                 }
                       imageCapture = ImageCapture.Builder().build()
-
             // kamera belakan digunakan sebagai kamera utama
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
             try {
                 cameraProvider.unbindAll()
                 cameraProvider.bindToLifecycle(
                     this, cameraSelector, preview, imageCapture)
-
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
-
         }, ContextCompat.getMainExecutor(this))
     }
-
     // Memberikan peringatan perizinan kamera terhadap pengguna
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
@@ -182,5 +158,3 @@ class MainActivity : AppCompatActivity() {
             }.toTypedArray()
     }
 }
-
-// Untuk video tidak saya tambahkan pak karena buildnya jadi luama buanget :)
